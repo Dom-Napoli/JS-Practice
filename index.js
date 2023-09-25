0a3de8d5b0a1cbbebaf6dc867cdff0f0aee51f7b
Dom-Napoli <Dom-Napoli@users.noreply.github.com>
2023-09-24 22:06:34 -0700

    Quokka now running

--- index.js
+++
@@ -1,8 +0,0 @@
-let meats=['beef', 'pork', 'checiken', 'fish', 'veal', 'bison'];
-let veggies=['potato','corn','carrot','bean'];
-let desserts=['pudding','pie','cake','cookie',;'ice-cream'];
-
-console.log(meats);
-
-
-
