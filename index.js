70195af927f0630053b8a0fe6b6b128538142ce9
Dom-Napoli <Dom-Napoli@users.noreply.github.com>
2023-09-25 17:43:31 -0700

    practice with outside tutor

---
+++ index.js
@@ -0,0 +1,40 @@
+// let newInventoryCarModels = ['S1', 'T1', 'S2', 'S1', 'S2', 'S1']
+// let newInventoryCarTypes = ['StockCar', 'Refrigerator', 'StockCar', 'Lorrie', 'Lorrie', 'Lorrie']
+// let newInventoryYearBuilt = [1922, 2011, 1975, 2002, 1965, 1951]
+
+// console.log(newInventoryCarTypes)
+// console.log(newInventoryYearBuilt)    
+
+
+// let i = 0;
+// let typeS1Count = 0;
+
+// while(i< newInventoryCarModels.length) {
+//     console.log(newInventoryCarModels[i]);
+//     if (newInventoryCarModels[i] === 'S1') {
+// typeS1Count++;
+//     }
+//     i++;
+// }
+// console.log("Number of S1 Cars is: " + typeS1Count);
+
+
+// const grid = [ 1, 2, 3, 4]
+// let oddNumbers = grid.filter
+
+
+
+// for(let i=0; i<grid.length; i++)
+//  {
+//  for(let j=0; j<grid [i].length; j++) {
+//     console.log(grid [i] [j])
+//  }
+//}
+
+// const ages = [32, 33, 16, 40];
+// const result = ages.filter(checkAdult);
+
+// function checkAdult(age) {
+//   return age >= 18;
+// }
+// console.log(result)
